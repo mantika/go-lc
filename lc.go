703c6a676b4db990c381633b2a3c7aea7a157cb0
Jonathan Leibiusky (@xetorthio) <ionathan@gmail.com>
2016-02-02 17:42:36 -0300

    Make LocalCopy public

--- lc.go
+++ lc.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-type localCopy struct {
+type LocalCopy struct {
 	data     map[string]interface{}
 	interval time.Duration
 	fillFunc func(*Handler)
@@ -14,7 +14,7 @@ type localCopy struct {
 }
 
 type Handler struct {
-	lc *localCopy
+	lc *LocalCopy
 }
 
 func (h *Handler) Get(key string) (interface{}, bool) {
@@ -28,26 +28,26 @@ func (h *Handler) Clean() {
 	h.lc.data = map[string]interface{}{}
 }
 
-func (lc *localCopy) Get(key string) (interface{}, bool) {
+func (lc *LocalCopy) Get(key string) (interface{}, bool) {
 	lc.lock.RLock()
 	i, o := lc.data[key]
 	lc.lock.RUnlock()
 	return i, o
 }
 
-func (lc *localCopy) Set(key string, value interface{}) {
+func (lc *LocalCopy) Set(key string, value interface{}) {
 	lc.lock.Lock()
 	lc.data[key] = value
 	lc.lock.Unlock()
 }
 
-func (lc *localCopy) Remove(key string) {
+func (lc *LocalCopy) Remove(key string) {
 	lc.lock.Lock()
 	delete(lc.data, key)
 	lc.lock.Unlock()
 }
 
-func (lc *localCopy) start() {
+func (lc *LocalCopy) start() {
 	lc.ticker = time.NewTicker(lc.interval)
 	go func() {
 		for {
@@ -59,19 +59,19 @@ func (lc *localCopy) start() {
 	}()
 }
 
-func (lc *localCopy) fill() {
+func (lc *LocalCopy) fill() {
 	lc.lock.Lock()
 	lc.fillFunc(&Handler{lc: lc})
 	lc.lock.Unlock()
 }
 
-func NewLocalCopy(interval time.Duration, fillFunc func(*Handler)) *localCopy {
-	lc := &localCopy{data: map[string]interface{}{}, interval: interval, fillFunc: fillFunc}
+func NewLocalCopy(interval time.Duration, fillFunc func(*Handler)) *LocalCopy {
+	lc := &LocalCopy{data: map[string]interface{}{}, interval: interval, fillFunc: fillFunc}
 	lc.start()
 	return lc
 }
-func NewImmediateLocalCopy(interval time.Duration, fillFunc func(*Handler)) *localCopy {
-	lc := &localCopy{data: map[string]interface{}{}, interval: interval, fillFunc: fillFunc}
+func NewImmediateLocalCopy(interval time.Duration, fillFunc func(*Handler)) *LocalCopy {
+	lc := &LocalCopy{data: map[string]interface{}{}, interval: interval, fillFunc: fillFunc}
 	lc.fill()
 	lc.start()
 	return lc
